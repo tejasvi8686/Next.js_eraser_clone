d0592d0a0a1053f15678fc9e388ffb6eca23ba34
tejasvi raj <tejasviraj8686@gmail.com>
2024-03-17 00:46:44 +0530

    landing page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+import Footer from "./_components/Footer";
 
 export default function Home() {
   return (
-   <div>
-    <h2>hey rtaj</h2>
-   </div>
+    <div>
+      <Header />
+
+      <Hero />
+
+      <Footer />
+    </div>
   );
 }
