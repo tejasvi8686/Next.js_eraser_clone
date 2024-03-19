8318475c5e95c6196c316d94b8c54c8049625e0c
tejasvi raj <tejasviraj8686@gmail.com>
2024-03-19 21:51:50 +0530

    setup convex

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,11 +1,23 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
-import React from "react";
+import { api } from "@/convex/_generated/api";
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { useQuery } from "convex/react";
+import React, { useEffect } from "react";
 
 const Dashboard = () => {
+  const {user}:any=useKindeBrowserClient()
+  const getUser=useQuery(api.user.getUser,{email:user?.email});
+  useEffect(()=>{
+    if(user)
+    {
+     console.log( getUser);
+    }
+},[user])
+
   return (
-    <div>Dashboard
+    <div>
+      Dashboard
       <Button>
         <LogoutLink>Logout</LogoutLink>
       </Button>
